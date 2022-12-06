c45ec762b1c343feb3f97bfa740d109cd85d73dc
Spiros Georgiladakis <sp.georgil@gmail.com>
2022-12-06 22:46:50 +0200

    Set axis to move and use hat for flips

--- main.py
+++ main.py
@@ -17,14 +17,28 @@ c = pygame.joystick.Joystick(0)
 c.init()
 logging.info("Controller connected")
 
-t = Tello()
+t = None
+use_drone = True
 
-t.connect()
-logging.info("Drone connected")
-logging.info(f"Battery remaining: {t.get_battery()}%")
-logging.info(f"Average temperature: {t.get_temperature()} Celcius")
+if use_drone:
+    t = Tello()
+    if t.connect():
+        logging.info("Drone connected")
+        logging.info(f"Battery remaining: {t.get_battery()}%")
+        logging.info(f"Average temperature: {t.get_temperature()} Celcius")
 
 is_drone_flying = False
+
+flip_dict = {
+    "x": {
+        1: "r",
+        -1: "l"
+    },
+    "y": {
+        1: "f",
+        -1: "b"
+    }
+}
 
 done = False
 prev_event_name = None
@@ -36,24 +50,36 @@ while not done:
             prev_event_name = event_name
             logging.debug(event_name)
 
+        if event.type == pygame.JOYAXISMOTION:
+            event.value = int(round(event.value, 2) * 10)
+            logging.info(f"Move intent detected: {event}")
+            if t is not None and event.value != 0:
+                t.go_xyz_speed(
+                    event.value if event.axis == 1 else 0,
+                    event.value if event.axis == 0 else 0,
+                    0,
+                    10
+                )
+                #if event.axis == 0:
+                #    t.move("left" if event.value < 0 else "right", 20 + abs(event.value))
+                #elif event.axis == 1:
+                #    t.move("forward" if event.value < 0 else "back", 20 + abs(event.value))
+
         if event.type == pygame.JOYHATMOTION:
-            logging.info(f"Motion intent detected: {event}")
-            move_directions = event.value
-            x_axis = move_directions[0]
-            y_axis = move_directions[1]
-            logging.info(f"Moving {move_directions}")
-            if x_axis > 0:
-                t.move_right(MOVE_STEP)
-            elif x_axis < 0:
-                t.move_left(MOVE_STEP)
-            if y_axis > 0:
-                t.move_forward(MOVE_STEP)
-            elif y_axis < 0:
-                t.move_back(MOVE_STEP)
+            logging.info(f"Flip intent detected: {event}")
+            flip_directions = event.value
+            x_flip = flip_directions[0]
+            y_flip = flip_directions[1]
+            if t is not None:
+                logging.info(f"Flipping {flip_directions}")
+                if x_flip in flip_dict["x"]:
+                    t.flip(flip_dict["x"][x_flip])
+                if y_flip in flip_dict["y"]:
+                    t.flip(flip_dict["y"][y_flip])
 
         if event.type == pygame.JOYBUTTONUP:
             logging.info(f"Key pressed: {event}")
-            if event.button == BTN_TRIANGLE:
+            if event.button == BTN_TRIANGLE and t is not None:
                 if not is_drone_flying:
                     logging.info("Trying liftoff...")
                     try:
