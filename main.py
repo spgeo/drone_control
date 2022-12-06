c63188808cd153efd6197bbe43aa16164304e0e3
Spiros Georgiladakis <sp.georgil@gmail.com>
2022-12-06 16:18:25 +0200

    Implement basic navigation and takeoff/land functions

--- main.py
+++ main.py
@@ -5,45 +5,75 @@ import time
 from djitellopy import Tello
 import pygame
 
-#logger = logging.Logger(__name__)
-class Logger():
-
-    def info(self, msg):
-        print(f"[INFO] {msg}")
-
-    def error(self, msg):
-        print(f"[ERROR] {msg}")
-
-
-logger = Logger()
-
-logger.info("Connecting the controller...")
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.info("Connecting the controller...")
 pygame.init()
-clock = pygame.time.Clock()
+#clock = pygame.time.Clock()
 
-joystick = {}
+BTN_TRIANGLE = 2
+MOVE_STEP = 40
 
-done = False
-while not done:
-    for event in pygame.event.get():
-        logger.info(event.type)
-
-exit()
+c = pygame.joystick.Joystick(0)
+c.init()
+logging.info("Controller connected")
 
 t = Tello()
 
 t.connect()
-logger.info("Drone successfully connected")
-logger.info(f"Battery remaining: {t.get_battery()}%")
-logger.info(f"Average temperature: {t.get_temperature()} Celcius")
+logging.info("Drone connected")
+logging.info(f"Battery remaining: {t.get_battery()}%")
+logging.info(f"Average temperature: {t.get_temperature()} Celcius")
 
-logger.info("Trying liftoff...")
-try:
-    t.takeoff()
-except Exception as err:
-    logger.error(f"Takeoff failed: {err}")
-logger.info("Drone is flying")
+is_drone_flying = False
 
-time.sleep(5)
-logger.info("Landing...")
-t.land()
+done = False
+prev_event_name = None
+move_directions = ()
+while not done:
+    for event in pygame.event.get():
+        event_name = pygame.event.event_name(event.type)
+        if event_name != prev_event_name:
+            prev_event_name = event_name
+            logging.debug(event_name)
+
+        if event.type == pygame.JOYHATMOTION:
+            logging.info(f"Motion intent detected: {event}")
+            move_directions = event.value
+            x_axis = move_directions[0]
+            y_axis = move_directions[1]
+            logging.info(f"Moving {move_directions}")
+            if x_axis > 0:
+                t.move_right(MOVE_STEP)
+            elif x_axis < 0:
+                t.move_left(MOVE_STEP)
+            if y_axis > 0:
+                t.move_forward(MOVE_STEP)
+            elif y_axis < 0:
+                t.move_back(MOVE_STEP)
+
+        if event.type == pygame.JOYBUTTONUP:
+            logging.info(f"Key pressed: {event}")
+            if event.button == BTN_TRIANGLE:
+                if not is_drone_flying:
+                    logging.info("Trying liftoff...")
+                    try:
+                        c.rumble(0, 1, 0)
+                        t.takeoff()
+                        logging.info("Drone is flying")
+                        is_drone_flying = True
+                    except Exception as err:
+                        logging.error(f"Takeoff failed: {err}")
+                    c.stop_rumble()
+                else:
+                    logging.info("Landing...")
+                    try:
+                        c.rumble(0, 1, 0)
+                        t.land()
+                        logging.info("Drone has landed")
+                        is_drone_flying = False
+                    except Exception as err:
+                        logging.error(f"Landing failed: {err}")
+                    c.stop_rumble()
+
+
+
